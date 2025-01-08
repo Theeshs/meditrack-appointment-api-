b4e3fc7f01f3b1de14d889dcd487fdeb9617bb23
thee <theekshana.sandaru@gmail.com>
2025-01-09 01:00:00 +0800

    updated

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,3 @@ docker build -t appointment-service-api:latest .
 
 echo "appliyng deployment yml"
 kubectl apply -f deployment.yaml
-
-echo "forwarding port"
-nohup kubectl port-forward svc/appointment-api-service 3002:3002 > port-forward.log 2>&1 &
